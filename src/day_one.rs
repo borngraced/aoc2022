41f86c8ac9ae4f7ff1fd0276e6d56cd7dd92adab
borngraced <samuelonoja970@gmail.com>
2022-12-02 09:41:32 +0100

    fix cargo clippy

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,10 +1,11 @@
 use crate::CALORIES;
 use once_cell::sync::Lazy;
 
+#[allow(dead_code)]
 pub(crate) fn find_elf_with_highest_calories(calories: &Lazy<Vec<&[i32]>>) -> (usize, i32) {
     let mut highest_elf_with_calories = (0, 0);
     calories.iter().enumerate().for_each(|(elf, cal)| {
-        let sum = cal.iter().fold(0, |acc, &x| acc + x);
+        let sum = cal.iter().sum::<i32>();
         if sum > highest_elf_with_calories.1 {
             highest_elf_with_calories = (elf, sum);
         }
@@ -12,6 +13,7 @@ pub(crate) fn find_elf_with_highest_calories(calories: &Lazy<Vec<&[i32]>>) -> (u
     highest_elf_with_calories
 }
 
+#[allow(dead_code)]
 pub(crate) unsafe fn find_top_elf_with_highest_calories() -> i32 {
     let mut hi_elf_c = 0;
     let mut count = 0;
